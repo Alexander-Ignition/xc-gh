a30677b39a50d54a162a218386f23d83a8f36a1f
Alexander Ignition <izh.sever@gmail.com>
2021-10-31 17:33:31 +0300

    Update bump version

---
+++ catbird.rb
@@ -0,0 +1,14 @@
+class Catbird < Formula
+  desc "Mock server for UI tests"
+  homepage "https://github.com/RedMadRobot/catbird"
+  url "https://github.com/RedMadRobot/catbird/releases/download/0.8.0/catbird.zip"
+  sha256 "a1d6c25da916c9e5336571e69137134a6f8c0c10d2ba05fc917e70531f7b72cd"
+
+  def install
+    bin.install "catbird"
+  end
+
+  test do
+    system "#{bin}/#{name}", "--help"
+  end
+end
